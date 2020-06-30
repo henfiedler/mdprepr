b77bb2e51aaf564ac02282ac56a1a89487e0fd9b
henfiedler <henfiedler@googlemail.com>
2020-06-30 16:03:41 +0200

    import dplyr for the package

--- R/befehle.R
+++ R/befehle.R
@@ -4,6 +4,7 @@
 #' @param a eine Variable
 #' @param b eine Variable
 #' @return Erzeugen neuer Variablen in \code{data} aus \code{a}, \code{b}, etc.
+#' @import dplyr
 bemerkung <- function(data, a, b) {
   dplyr::mutate(data, Sel_Bemerkung = paste0("Unterlagen: ",
                                       ifelse(grepl("Mail", a), "Mail", "Post"),
@@ -12,7 +13,7 @@ bemerkung <- function(data, a, b) {
 }
 
 selektion17 <- function(data, a) {
-  dplyr::rename(data, Sel_Selektion17 = all_of(a)) %>%
+  dplyr::rename(data, Sel_Selektion17 = dplyr::all_of(a)) %>%
     dplyr::mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "keins", "")) %>%
     dplyr::mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "Newsletter-Abo", "begrüßen"))
 }
