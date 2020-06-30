5b91540b5464fc7166a44ebd8c541ab945347014
henfiedler <henfiedler@googlemail.com>
2020-06-30 15:43:41 +0200

    add dplyr

--- R/befehle.R
+++ R/befehle.R
@@ -5,18 +5,18 @@
 #' @param b eine Variable
 #' @return Erzeugen neuer Variablen in \code{data} aus \code{a}, \code{b}, etc.
 bemerkung <- function(data, a, b) {
-  mutate(data, Sel_Bemerkung = paste0("Unterlagen: ",
+  dplyr::mutate(data, Sel_Bemerkung = paste0("Unterlagen: ",
                                       ifelse(grepl("Mail", a), "Mail", "Post"),
                                       ifelse(b != "",
                                              paste0(", Bem: ", b), "")))
 }
 
 selektion17 <- function(data, a) {
-  rename(data, Sel_Selektion17 = all_of(a)) %>%
-    mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "keins", "")) %>%
-    mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "Newsletter-Abo", "begrüßen"))
+  dplyr::rename(data, Sel_Selektion17 = all_of(a)) %>%
+    dplyr::mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "keins", "")) %>%
+    dplyr::mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "Newsletter-Abo", "begrüßen"))
 }
 
 zeitschrift <- function(data, a) {
-  mutate(data, Mitgl_digitale_Zeitschrift = ifelse(grepl("Mail", a), 1, 0))
+  dplyr::mutate(data, Mitgl_digitale_Zeitschrift = ifelse(grepl("Mail", a), 1, 0))
 }
