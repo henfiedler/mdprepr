07f4497ec94bd7a6b040f32bdb9e60d98ac39593
henfiedler <henfiedler@googlemail.com>
2020-06-30 15:14:19 +0200

    writing first roxygen documentation

--- R/befehle.R
+++ R/befehle.R
@@ -1,17 +1,22 @@
-bemerkung <- function(data, a, b)
-{ # erzeuge eine neue Variable "Sel_Bemerkung"
+#' Befehle für FileMaker-kompatible Aufbereitung
+#'
+#' @param data ein Ausgangsdatensatz.
+#' @param a eine Variable
+#' @param b eine Variable
+#' @return Erzeugen neuer Variablen in \code{data} aus \code{a}, \code{b}, etc.
+bemerkung <- function(data, a, b) {
   mutate(data, Sel_Bemerkung = paste0("Unterlagen: ",
                                       ifelse(grepl("Mail", a), "Mail", "Post"),
                                       ifelse(b != "",
                                              paste0(", Bem: ", b), "")))
 }
 
-selektion17 <- function(data, a)
-{rename(data, Sel_Selektion17 = all_of(a)) %>% # in den anderen Datensätzen "Weitere.Informationen"
+selektion17 <- function(data, a) {
+  rename(data, Sel_Selektion17 = all_of(a)) %>% # in den anderen Datensätzen "Weitere.Informationen"
     mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "keins", "")) %>%
     mutate(Sel_Selektion17 = replace(Sel_Selektion17, Sel_Selektion17 == "Newsletter-Abo", "begrüßen"))
 }
 
-zeitschrift <- function(data, a)
-{mutate(data, Mitgl_digitale_Zeitschrift = ifelse(grepl("Mail", a), 1, 0))
+zeitschrift <- function(data, a) {
+  mutate(data, Mitgl_digitale_Zeitschrift = ifelse(grepl("Mail", a), 1, 0))
 }
