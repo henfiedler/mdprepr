516c75f805cd14618530f451a5d5593babab1829
henfiedler <henfiedler@googlemail.com>
2020-07-02 13:44:21 +0200

    Kommentierung von gmail korrigieren

--- R/gmail.R
+++ R/gmail.R
@@ -1,8 +1,8 @@
-#' Erzeugen der Variable gmail
+#' Erzeugen der Variable Adr_e.mail
 #'
 #' @param data der Ausgangsdatensatz
 #' @param a erste Variable
-#' @return Alle Google-Mail Adressen in der Variable \code{a} werden zu gmail.com standardisiert
+#' @return Die Variable \code{a} wird zu Adr_e.Mail umbenannt und alle Google-Mail-Adressen werden zu gmail.com standardisiert
 #' @import dplyr
 #' @export
 gmail <- function(data, a) {
